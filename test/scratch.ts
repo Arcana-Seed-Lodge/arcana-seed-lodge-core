4c35ff4af84fde59a6e95c6a41444fd21fca4552
cmdruid <cscottdev@protonmail.com>
2025-05-19 16:58:10 -0500

    added regtest option to address generation

--- test/scratch.ts
+++ test/scratch.ts
@@ -1,8 +1,7 @@
 import { get_checksum } from '../src/lib/checksum.js'
 
 const key_descriptor = '[deadbeef/0h/1h/2h]xpub6ERApfZwUNrhLCkDtcHTcxd75RbzS1ed54G1LkBUHQVHQKqhMkhgbmJbZRkrgZw4koxb5JaHWkY4ALHY2grBGRjaDMzQLcgJvLJuZZvRcEL/<0;1>/*'
-
-const key_checksum = get_checksum(key_descriptor)
+const key_checksum   = get_checksum(key_descriptor)
 
 console.log('\nKey Descriptor Results:')
 console.log(key_descriptor + '#' + key_checksum)
